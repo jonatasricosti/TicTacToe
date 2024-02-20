4151477f2c6522d42a7f50168d2a685c71c7e7d3
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-02-19 21:29:04 -0300

    adicionei as telas do jogo, empate, vitória e a lógica pra saber se o 'X' ou 'O' ganhou

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
 
+// os estados de tela do jogo
+enum state
+{
+	TURN_X,
+	TURN_O,
+	X_WIN,
+	O_WIN,
+	TIE,
+};
+
+int estado = TURN_X;
+int round = 0;
+
+string status;
+
 // para o game loop
 bool executando = true;
 
 // esse array reprenta o tabuleiro
-char board[9] = {'X','X','O','X',0,0,0,0,'O'};
+char board[9];
 
 // essa função reseta o jogo
 void ResetGame()
@@ -13,6 +28,115 @@ void ResetGame()
 	for(int i = 0; i < 9; i++)
 	{
 		board[i] = 0;
+	}
+	
+	round = 0;
+}
+
+// essa função verifica se o x ganhou
+void checkXWin()
+{
+	if(board[0] == 'X' && board[1] == 'X' && board[2] == 'X')
+	{
+		status = "parabéns X ganhou na primeira linha horizontal";
+		estado = X_WIN;
+	}
+	
+	if(board[3] == 'X' && board[4] == 'X' && board[5] == 'X')
+	{
+		status = "parabéns X ganhou na segunda linha horizontal";
+		estado = X_WIN;
+	}
+	
+	if(board[6] == 'X' && board[7] == 'X' && board[8] == 'X')
+	{
+		status = "parabéns X ganhou na terceira linha horizontal";
+		estado = X_WIN;
+	}
+	
+	if(board[0] == 'X' && board[3] == 'X' && board[6] == 'X')
+	{
+		status = "parabéns X ganhou na primeira linha vertical";
+		estado = X_WIN;
+	}
+	
+	if(board[1] == 'X' && board[4] == 'X' && board[7] == 'X')
+	{
+		status = "parabéns X ganhou na segunda linha vertical";
+		estado = X_WIN;
+	}
+	
+	if(board[2] == 'X' && board[5] == 'X' && board[8] == 'X')
+	{
+		status = "parabéns X ganhou na terceira linha vertical";
+		estado = X_WIN;
+	}
+	
+	// diagonais
+	if(board[0] == 'X' && board[4] == 'X' && board[8] == 'X')
+	{
+		status = "parabéns X ganhou na primeira diagonal";
+		estado = X_WIN;
+	}
+	
+	if(board[2] == 'X' && board[4] == 'X' && board[6] == 'X')
+	{
+		status = "parabéns X ganhou na segunda diagonal";
+		estado = X_WIN;
+	}
+}
+
+
+// essa função verifica se o o ganhou
+void checkOWin()
+{
+	if(board[0] == 'O' && board[1] == 'O' && board[2] == 'O')
+	{
+		status = "parabéns O ganhou na primeira linha horizontal";
+		estado = O_WIN;
+	}
+	
+	if(board[3] == 'O' && board[4] == 'O' && board[5] == 'O')
+	{
+		status = "parabéns O ganhou na segunda linha horizontal";
+		estado = O_WIN;
+	}
+	
+	if(board[6] == 'O' && board[7] == 'O' && board[8] == 'O')
+	{
+		status = "parabéns O ganhou na terceira linha horizontal";
+		estado = O_WIN;
+	}
+	
+	if(board[0] == 'O' && board[3] == 'O' && board[6] == 'O')
+	{
+		status = "parabéns O ganhou na primeira linha vertical";
+		estado = O_WIN;
+	}
+	
+	if(board[1] == 'O' && board[4] == 'O' && board[7] == 'O')
+	{
+		status = "parabéns O ganhou na segunda linha vertical";
+		estado = O_WIN;
+	}
+	
+	if(board[2] == 'O' && board[5] == 'O' && board[8] == 'O')
+	{
+		status = "parabéns O ganhou na terceira linha vertical";
+		estado = O_WIN;
+	}
+	
+	// diagonais
+	if(board[0] == 'O' && board[4] == 'O' && board[8] == 'O')
+	{
+		status = "parabéns O ganhou na primeira diagonal";
+		estado = O_WIN;
+	}
+	
+	if(board[2] == 'O' && board[4] == 'O' && board[6] == 'O')
+	{
+		status = "parabéns O ganhou na segunda diagonal";
+		estado = O_WIN;
 	}
 }
 
@@ -41,23 +165,153 @@ void Drawboard()
 	}
 }
 
+
+// use essa função pra coloca 'X' no tabuleiro
+void PUT_X()
+{
+	cout << endl;
+	cout << "round: " << round << endl;
+	cout << "Vez do x" << endl;
+	cout << "digite um número de 0 até 8" << endl;
+	static int i = 0;
+	cin >> i;
+	
+	if(i < 0 || i > 8)
+	{
+		cout << "não temos essa posição" << endl;
+		estado = TURN_X;
+		return;
+	}
+	
+	if(board[i] == 0)
+	{
+		board[i] = 'X';
+		round++;
+		estado = TURN_O;
+	}
+	
+	else
+	{
+		cout << "temos valor aqui" << endl;
+		estado = TURN_X;
+	}
+}
+
+
+// use essa função coloca 'O' no tabuleiro
+void PUT_O()
+{
+	cout << endl;
+	cout << "round: " << round << endl;
+	cout << "Vez do o" << endl;
+	cout << "digite um número de 0 até 8" << endl;
+	static int i = 0;
+	cin >> i;
+	
+	if(i < 0 || i > 8)
+	{
+		cout << "não temos essa posição" << endl;
+		estado = TURN_O;
+		return;
+	}
+	
+	if(board[i] == 0)
+	{
+		board[i] = 'O';
+		round++;
+		estado = TURN_X;
+	}
+	
+	else
+	{
+		cout << "temos valor aqui" << endl;
+		estado = TURN_O;
+	}
+}
+
+void DrawTie()
+{
+	cout << endl;
+	cout << "oops parece que houve um empate" << endl;
+	cout << "deseja continuar: " << endl;
+	cout << "[1] sim" << endl;
+	cout << "[2] não" << endl;
+	static int reposta = 0;
+	cin >> reposta;
+	if(reposta == 1)
+	{
+		ResetGame();
+		estado = TURN_X;
+	}
+	
+	else
+	{
+		executando = false;
+	}
+}
+
+
+// essa função verifica se teve empate
+void CheckTie()
+{
+	if(round >= 9)
+	{
+		estado = TIE;
+	}
+}
+
+void DrawWin()
+{
+	cout << endl;
+	cout << status << endl;
+	cout << "deseja continuar: " << endl;
+	cout << "[1] sim" << endl;
+	cout << "[2] não" << endl;
+	static int reposta = 0;
+	cin >> reposta;
+	
+	if(reposta == 1)
+	{
+		ResetGame();
+		estado = TURN_X;
+	}
+	
+	else
+	{
+		executando = false;
+	}
+}
+
+// use essa função pra mudar a tela do jogo
+void RunGame()
+{
+	switch(estado)
+	{
+		case TURN_X: PUT_X(); break;
+		case TURN_O: PUT_O(); break;
+		case TIE: DrawTie(); break;
+		case X_WIN: DrawWin(); break;
+		case O_WIN: DrawWin(); break;
+		default: ;break;
+	}
+}
+
 int main()
 {
 setlocale(LC_ALL, "portuguese");
 
-//ResetGame();
-int lixo;
+ResetGame();
 
 // game loop
 while(executando)
 {
-	
+	checkXWin();
+	checkOWin();
 	Drawboard();
-	cout << endl;
-	cout << "digite um valor inútil pra sair do game loop: ";
-	cin >> lixo;
-	executando = false;
+	CheckTie();
+	RunGame();
 }
 
+system("pause");
 return 0;
 }
